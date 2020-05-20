9d2cb39d3aa1051cfb8efa77c4686ae962815b38
crissy aguilar <64777482+crissycancode@users.noreply.github.com>
2020-05-20 21:36:09 +0800

    Update TestFile.java

--- TestFile.java
+++ TestFile.java
@@ -1,7 +1,5 @@
 public class TestFile {
 	public static void main (String[] args){
 		System.out.println("Hello Github");
-		System.out.println("I cant understand");
-		System.out.println("A lot of stupid things");
 	}
 }
